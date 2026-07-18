791d0d5bd995f0a8eeec3b50ebf113ad45652cc4
Rawisara Joradon <285439080+rawisaradesign@users.noreply.github.com>
2026-07-18 14:02:52 +0700

    final_Design

--- weapon_shop/show_catalog.py
+++ weapon_shop/show_catalog.py
@@ -5,7 +5,9 @@ from data import weapons_catalog
 
 # TODO: สร้างฟังก์ชัน show_catalog()
 #   - print อาวุธทุกชิ้นใน weapons_catalog บรรทัดละชิ้น (รหัส, ชื่อ, ราคา, พลังโบนัส)
-
+def show_catalog():
+    for i in weapons_catalog:
+        print({i["1"]})
 
 # ทดสอบ: python -m weapon_shop.show_catalog
 if __name__ == "__main__":
