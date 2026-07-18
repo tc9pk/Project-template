73389dc253c07aa1d96f365b2bd65fbb4215a127
tanpak <thanyaphak2551@gmail.com>
2026-07-18 12:23:09 +0700

    show_catalog1

--- weapon_shop/show_catalog.py
+++ weapon_shop/show_catalog.py
@@ -6,6 +6,8 @@ from data import weapons_catalog
 # TODO: สร้างฟังก์ชัน show_catalog()
 #   - print อาวุธทุกชิ้นใน weapons_catalog บรรทัดละชิ้น (รหัส, ชื่อ, ราคา, พลังโบนัส)
 
+def show_catalog():
+    print(weapons_catalog)
 
 # ทดสอบ: python -m weapon_shop.show_catalog
 if __name__ == "__main__":
